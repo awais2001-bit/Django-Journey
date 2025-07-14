efb52bba089c4e40a26f53ee5593648bb0193c3b
MUHAMMAD AWAIS <awaismuhammad090@gmail.com>
2025-07-14 22:35:11 +0500

    Django template language

--- chap1/app4/urls.py
+++ chap1/app4/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app4.views import dynamic_view
+from app4.views import dynamic_view, template_language
 
 urlpatterns = [
     path('template/', dynamic_view, name='dynamic_view'),
+    path('temp_lang/', template_language, name='template_language'),
 ]
--- chap1/app4/views.py
+++ chap1/app4/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+
+#varibles example
 
 def dynamic_view(request):
     #business logic here
     profile = {'name':'awais'}
     return render(request, 'app4/index.html', context= profile)
+
+
+
+def template_language(request):
+    return render(request, 'app4/temp.html', context={
+        'name': 'Awais',
+        'age': 30,
+        'hobbies': ['coding', 'reading', 'gaming'],
+        'time': datetime.now(),
+        'float_value': 3.14159,
+        'nm':False,
+    })
+
+
