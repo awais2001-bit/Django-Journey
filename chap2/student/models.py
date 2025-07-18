fcfa2142d8da6b3992abb47738bb22c5788dccac
MUHAMMAD AWAIS <awaismuhammad090@gmail.com>
2025-07-18 17:57:16 +0500

    model file and migrations done

--- chap2/student/models.py
+++ chap2/student/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    city = models.CharField(max_length=100)
+    comments = models.TextField(blank=True, null=True, default='Nothing')
