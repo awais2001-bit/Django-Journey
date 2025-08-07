2a661ae846873d8ece744665c63abc031f66a0ce
MUHAMMAD AWAIS <awaismuhammad090@gmail.com>
2025-08-08 00:58:41 +0500

    messages in django done

--- chap5/teacher/views.py
+++ chap5/teacher/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from teacher.forms import StudentRegistration,TeacherRegistration
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +9,7 @@ def student_view_form(req):
         form = StudentRegistration(req.POST)
         if form.is_valid():
             form.save()
+            messages.success(req, 'Student registered successfully!')
     else:
         form = StudentRegistration()
     
@@ -18,6 +20,7 @@ def teacher_view_form(req):
         form = TeacherRegistration(req.POST)
         if form.is_valid():
             form.save()
+            messages.success(req, 'Teacher registered successfully!')
     else:
         form = TeacherRegistration()
     return render(req, 'teacher/teacherreg.html', {'form': form})
@@ -27,5 +30,10 @@ def home(req):
     return render(req, 'teacher/home.html', context)
 
 def profile(req,student_id):
+    messages.add_message(req, messages.SUCCESS, 'Profile loaded successfully!')
+    messages.add_message(req, messages.INFO, 'This is an info message.')
+    messages.success(req, 'This is a success message.')
+    messages,set_level(req, messages.DEBUG)
+    messages.debug(req, 'This is a debug message.') #debug messages are not shown by default in production, you hqave to set the value first and its level is 10 above 10 you can use by default
     student = {'id':student_id}
     return render(req, 'teacher/profile.html',student)
