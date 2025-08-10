127ac4f763e5f344f32e20eeb381e422b69cce41
MUHAMMAD AWAIS <awaismuhammad090@gmail.com>
2025-08-11 04:06:09 +0500

    Db with no Qset

--- chap6/school/views.py
+++ chap6/school/views.py
@@ -10,6 +10,8 @@ def student_data(req):
     #all_data = Student.objects.all()
     #all_data = Student.objects.filter(marks=1015)
     #all_data = Student.objects.exclude(marks=1015)
+    #all_data = Student.objects.latest('passdate') # latest pass date will be returned
+    #all_data.exists() # returns True if any record exists
     all_data = Student.objects.order_by('marks') # or '-marks' for descending order [0:5] top five will be shown
     return render(req, 'school/student_data.html', {'students': all_data})
 
