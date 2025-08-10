8f3ea82b82346b4600e191099c9dda9ca80833de
MUHAMMAD AWAIS <awaismuhammad090@gmail.com>
2025-08-11 04:20:08 +0500

    field lookups done

--- chap6/school/urls.py
+++ chap6/school/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from school.views import home,student_data
+from school.views import home,student_data,student_data_field_lookup
 
 urlpatterns = [
     path('', home, name='home'),
     path('student_data/', student_data, name='student_data'),
+    path('student_data_field_lookup/', student_data_field_lookup, name='student_data_field_lookup'),
 ]
--- chap6/school/views.py
+++ chap6/school/views.py
@@ -15,3 +15,14 @@ def student_data(req):
     all_data = Student.objects.order_by('marks') # or '-marks' for descending order [0:5] top five will be shown
     return render(req, 'school/student_data.html', {'students': all_data})
 
+def student_data_field_lookup(req):
+    all_data = Student.objects.filter(name__exact='Hamza') # exact match and also u can use iexact if you dont want case sensitive
+    #all_data = Student.objects.filter(name__contains='a') # this will show the data which contains 'a' in name
+    #all_data = Student.objects.filter(id_in=[1, 2, 3]) # this will show the data which has id in the list
+    #all_data = Student.objects.filter(marks_gt=1000) # this will show the data which has marks greater than 1000
+    #all_data = Student.objects.filter(name__startswith='a') # this will show the data which has name starting with 'a'
+     #all_data = Student.objects.filter(pass_data__range='') #for date
+    
+    
+    
+    return render(req, 'school/student_data.html', {'students': all_data})
