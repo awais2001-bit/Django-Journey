8fe0de9a0659524deb3498da2e3523c6c57b4588
MUHAMMAD AWAIS <awaismuhammad090@gmail.com>
2025-08-11 21:10:36 +0500

    generic list view

--- chap6/school/urls.py
+++ chap6/school/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from school.views import home,student_data,student_data_field_lookup
+from school.views import home,student_data,student_data_field_lookup,HomeView, StudentListView
 
 urlpatterns = [
     path('', home, name='home'),
     path('student_data/', student_data, name='student_data'),
     path('student_data_field_lookup/', student_data_field_lookup, name='student_data_field_lookup'),
+    path('home_view/', HomeView.as_view(), name='home_view'),
+    path('student_list/', StudentListView.as_view(), name='student_list'),
 ]
--- chap6/school/views.py
+++ chap6/school/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 from school.models import Student, Teacher
 from django.db.models import Q
+from django.views import View
+from django.views.generic.list import ListView
 # Create your views here.
 
 def home(req):
     return render(req, 'school/home.html')
 
+class HomeView(View):
+    def get(self, req):
+        return render(req, 'school/home.html')
+
 def student_data(req):
-    #all_data = Student.objects.all()
+    all_data = Student.objects.all()
     #all_data = Student.objects.filter(marks=1015)
     #all_data = Student.objects.exclude(marks=1015)
     #all_data = Student.objects.latest('passdate') # latest pass date will be returned
     #all_data.exists() # returns True if any record exists
-    all_data = Student.objects.order_by('marks') # or '-marks' for descending order [0:5] top five will be shown
+    #all_data = Student.objects.order_by('marks') # or '-marks' for descending order [0:5] top five will be shown
     return render(req, 'school/student_data.html', {'students': all_data})
+
+
+#generic based ListView is used when you want to show a list of objects in a template, the generic view reduces code and improves reusability
+class StudentListView(ListView):
+    model = Student
+    #template_name = 'school/student_list.html'  # specify the template to be used
+    def get_queryset(self):
+        return Student.objects.filter(marks=1015)
 
 def student_data_field_lookup(req):
     all_data = Student.objects.filter(name__exact='Hamza') # exact match and also u can use iexact if you dont want case sensitive
